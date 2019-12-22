3a68bb4b7e9753d06ebc6168f721e9a15c749dab
Scott Matsuda <smatsuda2014@gmail.com>
2019-12-22 16:31:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 def speak_to_grandma(string)
   if string.upcase()
-    puts "NO, NOT SINCE 1938!"
-  elsif
-  string == "I LOVE YOU GRANDMA!"
-    
+    return "NO, NOT SINCE 1938!"
+  elsif string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return  
 end
